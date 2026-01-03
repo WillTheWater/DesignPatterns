6b871d911d5837a9ce976e344fd8e1fe76da7b19
WillTheWater <willthewater@protonmail.com>
2026-01-03 16:24:27 +0100

    Added Structural Design Patterns

---
+++ include/FunctionLibrary/Demo.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "SOLID/SingleResponsibilityPrinciple.h"
+#include "SOLID/OpenClosedPrinciple.h"
+#include "SOLID/LiskovSubstitutionPrinciple.h"
+#include "SOLID/InterfaceSegregationPrinciple.h"
+#include "SOLID/DependencyInversionPrinciple.h"
+#include "Creational/Singleton.h"
+#include "Creational/FactoryMethod.h"
+#include "Creational/AbstractFactory.h"
+#include "Creational/Prototype.h"
+#include "Creational/Builder.h"
+
+void RunDemo()
+{
+    bool IsRunning = true;
+    while (IsRunning)
+    {
+        HFL::ClearScreen();
+        HFL::PrintMainMenu();
+        int Choice = HFL::GetValidMenuInput(10);
+
+        switch (Choice)
+        {
+        case 1:
+            SRP::RunDemo(); // Single Responsibility Principle Demo
+            break;
+        case 2:
+            OCP::RunDemo(); // Open-Closed Principle Demo
+            break;
+        case 3:
+            LSP::RunDemo(); // Liskov Substitution Principle Demo
+            break;
+        case 4:
+            ISP::RunDemo(); // Interface Segregation Principle Demo
+            break;
+        case 5:
+            DIP::RunDemo(); // Dependency Inversion Principle Demo
+            break;
+        case 6:
+            SGT::RunDemo(); // Singleton Demo
+            break;
+        case 7:
+            FTM::RunDemo(); // Factory Method Demo
+            break;
+        case 8:
+            AFT::RunDemo(); // Abstract Factory Demo
+            break;
+        case 9:
+            PRO::RunDemo(); // Prototype Demo
+            break;
+        case 10:
+            BLD::RunDemo(); // Builder Demo
+            break;
+        case 0:
+            IsRunning = false;
+            break;
+        }
+    }
+}
